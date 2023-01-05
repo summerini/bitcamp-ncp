bd30ded3834b4d63d0ddc93cdcd6f381eadde8cb
summerin <summerin.jeon@gmail.com>
2023-01-05 16:55:57 +0900

    0105

--- eomcs-java-lang/app/src/main/java/com/eomcs/oop/ex02/Exam0250.java
+++ eomcs-java-lang/app/src/main/java/com/eomcs/oop/ex02/Exam0250.java
@@ -55,27 +55,41 @@ public class Exam0250 {
     // 다음과 같이 각 식의 계산 결과를 보관할 메모리를 준비한다.
     Calculator c1 = new Calculator(); // 식1의 계산 결과를 보관할 메모리 준비
     Calculator c2 = new Calculator(); // 식2의 계산 결과를 보관할 메모리 준비
+    Calculator c3 = new Calculator(); // 식3의 계산 결과를 보관할 메모리 준비
+    Calculator c4 = new Calculator(); // 식4의 계산 결과를 보관할 메모리 준비
 
     // 계산을 수행할 때 계산 결과를 보관할 메모리를 전달한다.
     Calculator.plus(c1, 2); // + 2
     Calculator.plus(c2, 3); // + 3
+    Calculator.plus(c3, 3); // + 3
+    Calculator.plus(c4, 3); // + 3
 
     Calculator.plus(c1, 3); // + 2 + 3
     Calculator.multiple(c2, 2); // + 3 * 2
+    Calculator.multiple(c3, 2); // + 3 * 2
+    Calculator.multiple(c4, 2); // + 3 * 2
 
     Calculator.minus(c1, 1); // + 2 + 3 - 1
     Calculator.plus(c2, 7); // + 3 * 2 + 7
+    Calculator.plus(c3, 7); // + 3 * 2 + 7
+    Calculator.minus(c4, 1); // + 2 + 3 - 1
 
     Calculator.multiple(c1, 7); // + 2 + 3 - 1 * 7
     Calculator.divide(c2, 4); // + 3 * 2 + 7 / 4
+    Calculator.divide(c3, 4); // + 3 * 2 + 7 / 4
+    Calculator.divide(c4, 4); // + 3 * 2 + 7 / 4
 
     Calculator.divide(c1, 3); // + 2 + 3 - 1 * 7 / 3 = ?
     Calculator.minus(c2, 5); // + 3 * 2 + 7 / 4 - 5 = ?
+    Calculator.divide(c3, 3); // + 2 + 3 - 1 * 7 / 3 = ?
+    Calculator.minus(c4, 5); // + 3 * 2 + 7 / 4 - 5 = ?
 
     // 식1의 계산 결과는 c1 인스턴스의 result 변수에 들어 있고,
     // 식2의 계산 결과는 c2 인스턴스의 result 변수에 들어 있다.
     System.out.printf("c1.result = %d\n", c1.result);
     System.out.printf("c2.result = %d\n", c2.result);
+    System.out.printf("c2.result = %d\n", c3.result);
+    System.out.printf("c2.result = %d\n", c4.result);
   }
 }
 
