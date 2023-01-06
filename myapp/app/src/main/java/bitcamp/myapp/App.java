35c4e1df076e6378c4aaaaf4f1e59116b9a8d9f2
summerin <summerin.jeon@gmail.com>
2023-01-06 17:08:14 +0900

    0106

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -3,42 +3,52 @@ package bitcamp.myapp;
 public class App {
 
   public static void main(String[] args) {
-    while (true) {
-      System.out.println("1. 회원관리");
-      System.out.println("9. 종료");
+    goMainMenu();
+    System.out.println("안녕히 가세요!");
 
-      int mumuNo = Prompt.inputInt("메뉴> ");
-      if (mumuNo == 1) {
-        goMenu();
-      } else if (mumuNo == 9) {
-        System.out.println("바이바이");
-        break;
-      } else {
-        System.out.println("잘못된 메뉴입니다");
-      }
-    }
+    // 프로그램이 사용한 자원 해제하기
     Prompt.close();
-  }
-  static void goMenu() {
-    while (true) {
-      System.out.println("[회원관리]");
-      System.out.println("1. 등록");
-      System.out.println("2. 목록");
-      System.out.println("3. 조회");
-      System.out.println("4. 변경");
-      System.out.println("5. 삭제");
-      System.out.println("0. 이전");
+  } // main()
 
-      int mumuNo2 = Prompt.inputInt("[회원관리]");
-      if (mumuNo2 == 1) {
-        MemberHandler.inputMembers();
-      } else if (mumuNo2 == 2) {
-        MemberHandler.printMembers();
-      } else if (mumuNo2 == 0) {
+  private static void goMainMenu() {
+    // 일반 학생 목록을 저장할 메모리를 준비한다.
+    MemberHandler generalMemberHandler = new MemberHandler("일반학생");
+
+    // 국비지원 학생 목록을 저장할 메모리를 준비한다.
+    MemberHandler supportedMemberHandler = new MemberHandler("국비지원학생");
+
+    // 기업 위탁 교육생 목록을 저장할 메모리를 준비한다.
+    MemberHandler companyMemberHandler = new MemberHandler("위탁교육생");
+
+
+    while (true) {
+      System.out.println("1. 일반학생관리");
+      System.out.println("2. 국비지원학생관리");
+      System.out.println("3. 위탁교육생관리");
+      System.out.println("9. 종료");
+      int menuNo = Prompt.inputInt("메뉴> ");
+
+      if (menuNo == 1) {
+        generalMemberHandler.service();
+      } else if (menuNo == 2) {
+        supportedMemberHandler.service();
+      } else if (menuNo == 3) {
+        companyMemberHandler.service();
+      } else if (menuNo == 9) {
         break;
       } else {
-        System.out.println("잘못된 메뉴입니다");
+        System.out.println("잘못된 메뉴 번호 입니다.");
       }
     }
   }
+
 } // class App
+
+
+
+
+
+
+
+
+
