3420364618312bd5215d77f17f5fd921682f0105
summerin <summerin.jeon@gmail.com>
2023-02-28 17:11:46 +0900

    0228

---
+++ myapp/app-server/src/main/java/bitcamp/myapp/controller/BoardController.java
@@ -0,0 +1,159 @@
+package bitcamp.myapp.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import bitcamp.myapp.service.BoardService;
+import bitcamp.myapp.vo.Board;
+import bitcamp.myapp.vo.BoardFile;
+import bitcamp.myapp.vo.Member;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+
+  @Autowired private ServletContext servletContext;
+  @Autowired private BoardService boardService;
+
+  @GetMapping("form")
+  public String form() {
+    return "/board/form.jsp";
+  }
+
+  @PostMapping("insert")
+  public String insert(
+      Board board,
+      Part[] files,
+      Model model,
+      HttpSession session) {
+    try {
+      Member loginUser = (Member) session.getAttribute("loginUser");
+      Member writer = new Member();
+      writer.setNo(loginUser.getNo());
+      board.setWriter(writer);
+
+      List<BoardFile> boardFiles = new ArrayList<>();
+      for (Part part : files) {
+        if (part.getSize() == 0) {
+          continue;
+        }
+
+        String filename = UUID.randomUUID().toString();
+        part.write(servletContext.getRealPath("/board/upload/" + filename));
+
+        BoardFile boardFile = new BoardFile();
+        boardFile.setOriginalFilename(part.getSubmittedFileName());
+        boardFile.setFilepath(filename);
+        boardFile.setMimeType(part.getContentType());
+        boardFiles.add(boardFile);
+      }
+      board.setAttachedFiles(boardFiles);
+
+      boardService.add(board);
+
+    } catch (Exception e) {
+      e.printStackTrace();
+      model.addAttribute("error", "data");
+    }
+    return "/board/insert.jsp";
+  }
+
+  @GetMapping("list")
+  public String list(String keyword, Model model) {
+    model.addAttribute("boards", boardService.list(keyword));
+    return "/board/list.jsp";
+  }
+
+  @GetMapping("view")
+  public String view(int no, Model model) {
+    model.addAttribute("board", boardService.get(no));
+    return"/board/view.jsp";
+  }
+
+  @PostMapping("update")
+  public String update(
+      Board board,
+      Part[] files,
+      Model model,
+      HttpSession session) {
+    try {
+      Member loginUser = (Member) session.getAttribute("loginUser");
+      Board old = boardService.get(board.getNo());
+      if (old.getWriter().getNo() != loginUser.getNo()) {
+        return "redirect:../auth/fail";
+      }
+
+      List<BoardFile> boardFiles = new ArrayList<>();
+      for (Part part : files) {
+        if (part.getSize() == 0) {
+          continue;
+        }
+
+        String filename = UUID.randomUUID().toString();
+        part.write(servletContext.getRealPath("/board/upload/" + filename));
+
+        BoardFile boardFile = new BoardFile();
+        boardFile.setOriginalFilename(part.getSubmittedFileName());
+        boardFile.setFilepath(filename);
+        boardFile.setMimeType(part.getContentType());
+        boardFile.setBoardNo(board.getNo());
+        boardFiles.add(boardFile);
+      }
+      board.setAttachedFiles(boardFiles);
+
+      boardService.update(board);
+
+    }  catch (Exception e) {
+      e.printStackTrace();
+      model.addAttribute("error", "data");
+    }
+
+    return "/board/update.jsp";
+  }
+
+  @PostMapping("delete")
+  public String delete(int no, Model model, HttpSession session) {
+    try {
+      Member loginUser = (Member) session.getAttribute("loginUser");
+
+      Board old = boardService.get(no);
+      if (old.getWriter().getNo() != loginUser.getNo()) {
+        return "redirect:../auth/fail";
+      }
+      boardService.delete(no);
+
+    }  catch (Exception e) {
+      e.printStackTrace();
+      model.addAttribute("error", "data");
+    }
+    return "/board/delete.jsp";
+  }
+
+  @GetMapping("filedelete")
+  public String filedelete(
+      int boardNo,
+      int fileNo,
+      HttpSession session) {
+
+    Member loginUser = (Member) session.getAttribute("loginUser");
+
+    Board old = boardService.get(boardNo);
+    if (old.getWriter().getNo() != loginUser.getNo()) {
+      return "redirect:../auth/fail";
+    } else {
+      boardService.deleteFile(fileNo);
+      return "redirect:view?no=" + boardNo;
+    }
+  }
+
+}
+
